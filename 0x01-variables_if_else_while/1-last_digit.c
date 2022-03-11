d0b4f332433ded4ba3ee817f238b0efc6cc1fdef
MAHAM <abdulkabore@gmail.com>
2022-03-11 06:21:05 -0800

    fix last digit and add alphabets

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,23 +9,23 @@
 int main(void)
 {
 	int n;
-	int last_number;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_number = n % 10;
+	last = n % 10;
 
-	if (last_number == 0)
+	if (last == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_number);
+		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-	else if (last_number != 0 && last_number < 6)
+	else if (last != 0 && last < 6)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_number);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_number);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	}
 	return (0);
 }
