cca6a9827213001663be19edb01aadcd4e9ff8a3
MAHAM <abdulkabore@gmail.com>
2022-03-11 09:21:56 -0800

    i'm so tired

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - description here
+ * Return: 0;
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	j = 0;
+	while (j < 10)
+	{
+		i = 0;
+		while (i < 10)
+		{
+			if (j != i && j < i)
+			{
+				putchar('0' + j);
+				putchar('0' + i);
+
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			i++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
+
