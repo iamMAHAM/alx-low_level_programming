6332caa0534b2708d86061f4f33bb651194e7f73
MAHAM <abdulkabore@gmail.com>
2022-03-11 08:53:53 -0800

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - description here
+ * Return: 0;
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		putchar('0' + i);
+		if (ch != 9 )
+		{
+			putchar(",");
+			putchar(" ");
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
