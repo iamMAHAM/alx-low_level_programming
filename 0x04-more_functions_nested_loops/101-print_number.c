21e245089a9c33f6fe6f85476cec2b508fddfb2d
MAHAM <abdulkabore@gmail.com>
2022-03-17 22:59:33 +0100

    ending project

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,18 +1,36 @@
 #include "main.h"
 
 /**
- * print_numbers - print first 10 digits
- * Return: 0 through 9
+ * print_number - print first 10 digits
+ * @n: an interger to print
+ * Return: 0 through n
  */
 
-void print_numbers(void)
+void print_number(int n)
 {
-	int i = 0;
+	unsigned int m, d, count;
 
-	while (i < 10)
+	if (n < 0)
 	{
-		_putchar('0' + i);
-		i++;
+		_putchar(45);
+		m = n * -1;
 	}
-	_putchar('\n');
+	else
+	{
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
