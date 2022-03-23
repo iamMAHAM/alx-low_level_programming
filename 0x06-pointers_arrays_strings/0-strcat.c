538cb39d32fc47cdf9f591ca119e7b2f009ca7ef
MAHAM <abdulkabore@gmail.com>
2022-03-23 15:25:40 -0700

    two string concatenator :)

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - function to concatenate two strings
+ * @dest: final string
+ * @src: source string
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+
+	while (dest[i] != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (dest);
+}
+
