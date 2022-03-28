afae39e895647b3c153d694d287b59ee5dcd5c1b
MAHAM <abdulkabore@gmail.com>
2022-03-28 18:14:36 +0200

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - search a match string 
+ * @s: a pointer to string
+ * @accept: a pointer to searched string
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	char *t = accept;
+
+	while (*s++)
+	{
+		while (*accept++)
+			if (*(s - 1) == *(accept - 1))
+			{
+				c++;
+				break;
+			}
+		if (!(*--accept))
+			break;
+		accept = t;
+	}
+	return (c);
+}
