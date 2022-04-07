3a846fc57f374e1256538ce2a9d96510ca995d1d
MAHAM <abdulkabore@gmail.com>
2022-04-07 15:37:44 +0200

    fix betty style

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * malloc_checked - malloc or return 98
+ * @b: value to be allowed
+ * Return: pointer or 98
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = NULL;
