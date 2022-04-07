7ff6f032c3bbdd043510cfef09cb17a0b0cea2a2
MAHAM <abdulkabore@gmail.com>
2022-04-07 19:42:09 +0200

    fix betty style and docs

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 /**
- * array_range - allocates memory for an array
- * @nmemb: number of element
- * @size: type of element
+ * array_range - generate an array
+ * @min: minimum
+ * @max: maximum
  * Return: pointer
  */
 int *array_range(int min, int max)
@@ -15,12 +15,12 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	
+
 	total_numbers = max - min + 1;
 	ptr = malloc(sizeof(int) * total_numbers);
 	if (ptr == NULL)
 		return (NULL);
-	
+
 	for (; i <= max; i++)
 	{
 		ptr[i] = min + i;
