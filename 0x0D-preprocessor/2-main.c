60e681c674098c86d943abec84be019f358452da
MAHAM <abdulkabore@gmail.com>
2022-04-11 23:41:55 +0200

    remove <unistd> migrate to __BASE_FILE__

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,35 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - prints the name of the file it was compiled from
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Everything worked ok
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
-/**
- * main - AN entry point
- * Return: 0  on success
- */
 int main(void)
 {
-	int i = 0, length = 0;
-
-	for (; __FILE__[i] != '\0'; i++)
-	{
-		length++;
-	}
-	i = 0;
-	while (i < length)
-	{
-		_putchar(__FILE__[i]);
-		i++;
-	}
-	_putchar('\n');
+	printf("%s\n", __BASE_FILE__);
 	return (0);
 }
