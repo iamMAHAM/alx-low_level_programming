2626e05c75d984877dfe57306f35b222792becf7
MAHAM <abdulkabore@gmail.com>
2022-04-12 23:52:50 +0200

    fix null struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * init_dog - init the dog
  * @d: the struct
@@ -8,6 +10,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		exit(1);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
