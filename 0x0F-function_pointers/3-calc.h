824c8195eb45d65d76fd19a5919f6c9408be155e
MAHAM <abdulkabore@gmail.com>
2022-04-13 16:39:45 +0200

    3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -5,14 +5,13 @@
 #include <stdlib.h>
 /**
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
