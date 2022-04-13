637c0e4379e5568388d340072238ee0267bfc8e9
MAHAM <abdulkabore@gmail.com>
2022-04-13 16:36:10 +0200

    all operators

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - a function to add to number
+ * @a: param a
+ * @b: param b
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - a function to add to number
+ * @a: param a
+ * @b: param b
+ * Return: a + b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - a function to multipy a and b
+ * @a: param a
+ * @b: param b
+ * Return: a + b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - a function to divide a and b
+ * @a: param a
+ * @b: param b
+ * Return: a + b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - a function that return the rest
+ * @a: param a
+ * @b: param b
+ * Return: a + b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
