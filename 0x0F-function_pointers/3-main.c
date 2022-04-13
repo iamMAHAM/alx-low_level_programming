ff235e905c21f6ddb0a73770d6370542afe994b1
MAHAM <abdulkabore@gmail.com>
2022-04-13 16:36:01 +0200

    main_func

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - An entry point
+ * 
+ * @argc: number of arugument
+ * @argv: an array of char
+ * Return: a result
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*pointer)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	pointer = get_op_func(argv[2]);
+
+	if (pointer == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = pointer(a, b);
+
+	printf("%d\n", result);
+	return (0);
+}
