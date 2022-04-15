7d0485b9b6e7e80a38e22cb1aee6cdc4f8bbcfd6
MAHAM <abdulkabore@gmail.com>
2022-04-15 16:39:39 +0200

    reinit code

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,16 +13,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	va_list args;
 
+
 	va_start(args, n);
-	for (; i < n; i++)
+	if (n > 0)
 	{
-		if (i == n || i == 0 || separator == NULL)
-			printf("%d", va_arg(args, int));
-		else
+		for (; i < n; i++)
 		{
-			printf("%s%d", separator, va_arg(args, int));
+			printf("%d", va_arg(args, int));
+			if ((i != n - 1))
+				printf("%s", separator);
 		}
 	}
+
 	printf("\n");
 	va_end(args);
 }
