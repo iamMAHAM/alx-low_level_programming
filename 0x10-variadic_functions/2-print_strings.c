5a3dca39acf5d73b218292a0aa66c65b791cd01a
MAHAM <abdulkabore@gmail.com>
2022-04-15 17:00:53 +0200

    fix error wile printed nil

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,13 +12,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
 	va_list args;
+	char *str;
 
 	va_start(args, n);
 	if (n > 0)
 	{
 		for (; i < n; i++)
 		{
-			printf("%s", va_arg(args, char *));
+			str = va_arg(args, char *);
+			if (str == NULL)
+				printf("nil");
+			else
+				printf("%s", str);
 			if ((i != n - 1) && separator != NULL)
 			{
 				printf("%s", separator);
