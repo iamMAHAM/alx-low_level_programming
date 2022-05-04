863e8802c193db4505cd74e30c00d2e6e795cf53
MAHAM <abdulkabore@gmail.com>
2022-05-04 12:10:11 +0000

    fix return 1 when filename is NULL

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (filename == NULL)
-		return (1);
+		return (-1);
 
 	length  = strlen(text_content);
 	status = write(fd, text_content, length);
