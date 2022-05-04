f104af7f914eb954e4aa4aea64510c38205be1d1
MAHAM <abdulkabore@gmail.com>
2022-05-04 11:41:32 +0000

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include "main.h"
+
+/**
+ * create_file - Create a file
+ * @filename: the file name
+ * @text_content: the text content
+ * Return: 1 on success otherwise -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, status, length;
+
+	if (filename == NULL)
+		return (-1);
+
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+	
+	length = strlen(text_content);
+	printf("%d", length);
+	status = write(fd, text_content, length);
+
+	if (status == -1)
+		return (-1);
+	
+	// close(fd);
+	return (1);
+}
