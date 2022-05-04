b385d197d458832dab08eefd89271298bb20dafd
MAHAM <abdulkabore@gmail.com>
2022-05-04 15:04:27 +0000

    try to fix error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,26 +17,21 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, status, length;
 
-	if (!filename) /*filename == NULL*/
-		return (-1);
-
-
-	fd = open(filename, O_WRONLY | O_APPEND);
-
-	if (fd == -1)
-		return (-1);
-
 	if (filename == NULL)
 		return (-1);
 
-	
-	for (length = 0; text_content[length] != 0; length++)
-		;
-	status = write(fd, text_content, length);
-
-	if (status == -1) /*permission error or other*/
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
+
+	for (length = 0; text_content[length] != 0; length++)
+		;
+
+	status = write(fd, text_content, length);
 	close(fd);
-	return (1);
+
+	return (status == -1 ? -1 : 1);
 }
