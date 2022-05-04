446cfe9263d3434aa92a2e2e790656dbc0313369
MAHAM <abdulkabore@gmail.com>
2022-05-04 12:57:41 +0000

    add null terminated byte

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,6 +30,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	length  = strlen(text_content);
+	text_content[length + 1] = '\0';
 	status = write(fd, text_content, length);
 
 	if (status == -1) /*permission error or other*/
