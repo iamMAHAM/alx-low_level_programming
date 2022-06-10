a7f7b1608dd61de040abeb50b4cdc3d94bf8e5f5
MAHAM <abdulkabore@gmail.com>
2022-06-10 08:49:45 +0000

    fix betty doc

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - add node end 
+ * add_dnodeint_end - add node end
  * @head: the head
  * @n: the number
  * Return: added node
@@ -13,11 +13,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (!head)
 		return (NULL);
-	
+
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
-	
+
 	new->n = n;
 	new->next = NULL;
 
@@ -30,7 +30,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	while (current->next)
 		current = current->next;
-	
+
 	current->next = new;
 	new->prev = current;
 
