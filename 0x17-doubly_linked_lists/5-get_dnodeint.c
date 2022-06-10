d6f3547ddd24d4d8f447e68d992d252e450a4ff2
MAHAM <abdulkabore@gmail.com>
2022-06-11 00:26:57 +0200

    update

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * get_dcurrentint_at_index - returns the nth current of a linked list.
- * @head: the head.
+ * get_dnodeint_at_index - returns the nth current of a linked list.
+ * @head: the head. 
  * @index: index.
  * Return: nth current, if the current doesn't exist, returns NULL.
  **/
-dlistint_t *get_dcurrentint_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *current = head;
 	unsigned int cont = 0;
