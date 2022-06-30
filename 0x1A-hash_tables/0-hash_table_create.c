3bc3cfdab7d4418e7dc52fadf65025dbf1ca500c
MAHAM <abdulkabore@gmail.com>
2022-06-30 12:17:03 +0200

    0_hash

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a new hash tables 
+ *
+ * @size: the size of hash table
+ * Return: a pointer to hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(size));
+	if (!new)
+		return (NULL);
+
+	return (new);
+}
